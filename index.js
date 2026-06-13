79f5e279cd7766fea1e90a789828179718ede6cd
Naomi Woldu <naomifrigginwoldu@Naomis-MacBook-Pro.local>
2026-06-13 17:06:41 -0400

    Update model viewer

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
-camera.position.z = 3;
+const camera = new THREE.PerspectiveCamera(50, w / h, 0.1, 1000);
+camera.position.z = 5;
 // make canvas transparent
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
